6f67bda44c917e022216879693999ec6efe26b17
Tejash <36923392+tejash023@users.noreply.github.com>
2019-07-08 15:23:12 +0530

    Test code pushed to repository.

---
+++ Printing the given number in words/Main.java
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+  int x,y,z;
+  scanf("%d",&x);
+  if(x==1)
+  printf("one");
+  else if(x==2)
+    printf("two");
+  else if(x==3)
+    printf("Three");
+  else if(x==4)
+    printf("four");
+  else if(x==5)
+    printf("Five");
+  else
+    printf("Invalid");
+  
+  return 0;
+}
